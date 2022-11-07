1046a558697f3eba119b435f11e859df6d2d5780
José Gaspar Sánchez García Dpto Info <jg.sanchezgarcia@edu.gva.es>
2022-11-07 18:04:17 +0100

    Pytest funciona en local

--- tests/test_sample.py
+++ tests/test_sample.py
@@ -1,15 +1,15 @@
 # This Python file uses the following encoding: utf-8
 import os, sys
 
-from ..sample import uno, zero, saludar
+import sample
 
 # Ejemplo de fichero de pruebas pytest
 # Autor: José Gaspar Sánchez García
 
 def test_uno():
-    assert uno() == 1
+    assert sample.uno() == 1
 
 def test_zero():
-    assert zero() == 0
+    assert sample.zero() == 0
 def test_saludar():
-    assert saludar() == "Hola mundo"
+    assert sample.saludar() == "Hola mundo"
